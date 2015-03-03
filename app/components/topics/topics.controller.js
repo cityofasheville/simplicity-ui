3c01a8b520dd675d2f4a93a8a8e7814107bef531
carlyleec <carlyleec@gmail.com>
2015-03-02 23:40:47 -0500

    fixes and changes for issues 41, 49, 50, 51, and 54. Didn't address issue 52, see issue comments

--- app/components/topics/topics.controller.js
+++ app/components/topics/topics.controller.js
@@ -1,5 +1,5 @@
-app.controller('TopicsCtrl', ['$scope', '$stateParams', '$state', 'Topics',
- function ($scope, $stateParams, $state, Topics) {
+app.controller('TopicsCtrl', ['$scope', '$stateParams', '$state', 'Topics', 'Backend',
+ function ($scope, $stateParams, $state, Topics, Backend) {
     $("html, body").animate({'scrollTop' : "0px"});
     $scope.$on('$stateChangeSuccess', function (event, toState) {
         if (toState.name === 'main.topics') {           
@@ -10,24 +10,29 @@ app.controller('TopicsCtrl', ['$scope', '$stateParams', '$state', 'Topics',
     });
 
     //you can't have more tha
-    $scope.heading = 'What are you looking for...';
-    if($stateParams.searchby === 'address'){
-      $scope.heading =  'What are you looking for at ';
-    }else if($stateParams.searchby === 'street_name'){
-      $scope.heading = 'What are you looking for on ';
-    }else if($stateParams.searchby === 'neighborhood'){
-      $scope.heading = 'What are you looking for in ';
-    }else if($stateParams.searchby === 'pinnum'){
-      $scope.heading = 'What are you looking for with the PIN ';
-    }else if($stateParams.searchby === 'owner_name'){
-      $scope.heading = 'What are you looking for by the owner ';
-    }
+    $scope.heading = 'Topics for ';
+    
 
     $scope.searchText = $stateParams.searchtext + '?';
 
     $scope.topics = Topics.getTopics($stateParams);
     
-
+    $scope.inTheCity = false;
+    $scope.loading = false;
+    $scope.anAddress = false;
+    if($stateParams.searchby === 'address'){
+      $scope.anAddress = true;
+      $scope.loading = true;
+      Backend.dataCache()
+      .then(function(data){
+        $scope.loading = false;
+        if(data.inTheCity === true){
+          $scope.inTheCity = true;
+        }else{
+          $scope.inTheCity = false;
+        }
+      });
+    }
 
 
 }]);
